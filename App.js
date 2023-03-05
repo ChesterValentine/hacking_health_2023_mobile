09b3669cadd126755b8d2739cfbe833d40b39505
ChesterValentine <loic.guesdon@gmail.com>
2023-03-05 15:19:56 +0100

    Add existing project files to Git

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="increment"></Button>
+      <Image source={{uri: 'https://reactjs.org/logo-og.png'}}
+       style={{width: 40, height: 40}} />
+       <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
       <StatusBar style="auto" />
     </View>
   );
